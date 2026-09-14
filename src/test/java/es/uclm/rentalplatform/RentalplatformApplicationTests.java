1ed30f165c74b9daeccc2c683cda32413dc75888
Anaís Chiquito Patiño <Anais.Chiquito@alu.uclm.es>
2026-09-14 15:54:25 +0200

    Add initial Spring Boot project structure

---
+++ src/test/java/es/uclm/rentalplatform/RentalplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uclm.rentalplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
